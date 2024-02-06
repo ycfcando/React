80eca001e533a11a27393dc31707aebfaf27c7a5
yechaofan <yechaofan@bytedance.com>
2024-02-06 17:49:39 +0800

    feat: eslint

--- src/pages/basic_layout/index.tsx
+++ src/pages/basic_layout/index.tsx
@@ -1,13 +1,13 @@
-import { FC } from 'react';
-import { navRead } from "@/utils/read";
-import style from './index.css';
-import svg from '@/svg/test.svg';
+import { FC } from 'react'
+import { navRead } from '@/utils/read'
+import style from './index.css'
+import svg from '@/svg/test.svg'
 
-export default function(): FC {
+export default function (): FC {
   return (
     <div className={style.ceshiCss}>
       {navRead('测试组件')}
       <img src={svg} />
     </div>
-  );
+  )
 };
